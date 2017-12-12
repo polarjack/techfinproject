747ff6dcf0e03653ad44fe42481058eec3881cc1
polarjack <luckyjack85@gmail.com>
2017-12-12 14:34:47 +0800

    intro page

--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('main/index', { title: 'Main' });
 });
 
+router.get('/intro', function(req, res) {
+  res.render('main/intro', { title: 'Intro'})
+})
+
+router.get('/travel', function(req, res) {
+  res.send("travel")
+})
+
+router.get('/host', function(req, res) {
+  res.send("host")
+})
+
+
+
 module.exports = router;
