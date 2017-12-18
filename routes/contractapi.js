bcec623d0e73fa22c0b4b0fe458637582e9a84f8
polarjack <luckyjack85@gmail.com>
2017-12-18 16:08:29 +0800

    partial

--- routes/contractapi.js
+++ routes/contractapi.js
@@ -14,9 +14,37 @@ router.get('/testing', function(req, res) {
 })
 
 router.get('/showinit', function(req, res) {
-  
-  res.send("this is a message")
+  var todo = agreement.showInit()
+  res.send(todo)
 })
 
+router.get('/showtime', function(req, res) {
+  var todo = agreement.showTime()
+  res.send(todo)
+})
+
+router.get('/showcurrentblock', function(req, res) {
+  var todo = agreement.showCurrentBlock()
+  res.send(todo)
+})
+
+router.get('/showcurrenttime', function(req, res) {
+  var todo = agreement.showCurrentTime()
+  var human_time = new Date(todo*1000);
+
+
+  // var human_year = human_time.getFullYear();
+  // var 
+  // var human_Date = human_time.getDate();
+  
+  // var hours = human_time.getHours();
+  // var minutes = "0" + human_time.getMinutes();
+  // var seconds = "0" + human_time.getSeconds();
+
+  // const toshow = human_year + " " + human_Date + "  ";
+  // const times = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  res.send(human_time.toISOString())
+
+})
 
 module.exports = router;
--- routes/items.js
+++ routes/items.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var doquery = require('../config/dbconfig');
+
 
 router.get('/', function(req, res) {
   res.send('items index pages')
 })
 
+router.get('/list', function(req, res) {
+  var toquery = "select * from items"
+  var todo = doquery(toquery, "");
+
+  todo.then(input => {
+    res.render('/items/list', {
+      title: "items list",
+      data: input
+    })
+  }).catch(input => {
+    console.log(input)
+  })
+})
+
+router.get('/insert', function(req, res) {
+
+})
+
+router.get('/update', function(req, res) {
+
+})
 
 module.exports = router;
